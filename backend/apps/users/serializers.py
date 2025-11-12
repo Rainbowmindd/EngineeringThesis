81844e25e840433e158cec7487be6d16e8fb5b3d
MartynaK <martyna.kindrat@gmail.com>
2025-11-12 18:20:40 +0100

    Dziala rejestracja

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
-User=get_user_model()
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    email=serializers.EmailField(
+    email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
@@ -15,23 +15,28 @@ class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model=User
+        model = User
         fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'role')
-        # extra_kwargs = {
-        #     'first_name': {'required': True},
-        #     'last_name': {'required': True},
-        # }
-        def validate(selfself, attrs):
-            if attrs['password'] != attrs['password2']:
-                raise serializers.ValidationError({"password": "Password fields didn't match."})
-            return attrs
 
-        def create (self, validated_data):
-            validated_data.pop('password2')
-            user = User.objects.create_user(**validated_data)
-            return user
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            role=validated_data.get('role', 'student'),
+        )
+        return user
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User
+        model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'role')
