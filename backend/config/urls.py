e2fd42cc97f0da388c50863d0cdc303eae842517
Rainbowmindd <martyna.kindrat@gmail.com>
2025-12-04 18:20:08 +0100

    Backend resetowania hasla

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,4 @@
+from dj_rest_auth.views import PasswordResetConfirmView
 from django.contrib import admin
 from django.urls import path,include
 
@@ -6,7 +7,11 @@ urlpatterns = [
     path('api/users/', include('apps.users.urls')),
     path('api/reservations/', include('apps.reservations.urls')),
     path('api/schedules/', include('apps.schedules.urls')),
-    path('api/notifications/', include('apps.notifications.urls'))
+    path('api/notifications/', include('apps.notifications.urls')),
+    #authentication endpoints
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/password/reset/confirm/<uid64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 
 ]
