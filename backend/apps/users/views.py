584d91c2732ac9e294537e6bbed6baa35e9755ee
Rainbowmindd <martyna.kindrat@gmail.com>
2025-12-07 19:17:42 +0100

    Revert "Konteneryzacja"
    This reverts commit 5fee39fc43e41869d2dd46c364394b07e8592830.

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -24,7 +24,7 @@ class ProfileView(generics.RetrieveAPIView):
         return self.request.user
 
 class LoginView(APIView):
-    permission_classes=[permissions.AllowAny]
+    permission_lasses=[permissions.AllowAny]
 
     def post(self,request):
         email=request.data.get('email')
