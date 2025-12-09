0ce629b82bd5accbd0688fd634e4c26c5d124a9d
Rainbowmindd <martyna.kindrat@gmail.com>
2025-12-09 14:39:58 +0100

    front3

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:8000/", //for backend connection
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://djangopracadyplomowa.azurewebsites.net/",
     headers: { 
         "Content-Type": "application/json",
     },
